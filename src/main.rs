2858b3af863f29e19db2476534ee569c1dafc2f5
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2024-11-26 13:31:45 +0900

    feat: implement axum HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use serde::Deserialize;
+use tokio::net::TcpListener;
+
+#[derive(Deserialize)]
+pub struct Config {
+    #[serde(default = "String::default")]
+    pub port: String,
+    #[serde(default = "String::default")]
+    pub writer_database_url: String,
+    #[serde(default = "String::default")]
+    pub reader_database_url: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let config =
+        envy::from_env::<Config>().map_err(|e| anyhow::anyhow!("failed to load config: {}", e))?;
+
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", &config.port))
+        .await
+        .map_err(|e| anyhow::anyhow!("failed to bind port: {}", e))?;
+
+    let router = Router::new();
+
+    axum::serve(listener, router)
+        .await
+        .map_err(|e| anyhow::anyhow!("failed to run axum router: {}", e))?;
+
+    Ok(())
 }
